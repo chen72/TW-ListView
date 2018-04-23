6a75e95feb5e72ab940b9a9107948dd90cbba6bb
16046469 <jie1n31997>
2018-04-23 13:17:19 +0800

    module.java

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -21,8 +21,16 @@ public class FirstActivity extends AppCompatActivity {
 
         al = new ArrayList<String>();
         al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+
+        aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, al);
 
 
+        lv = (ListView)findViewById(R.id.lvYear);
+
+        lv.setAdapter(aa);
 
 
 
